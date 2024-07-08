e60467de7f6681d3da63c63e1a2195de0da032ae
io <szpaszek@gmail.com>
2024-07-08 23:00:11 +0200

    initial commit

---
+++ NamingScheme.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace VesselAutoRenamer
+{
+    public abstract class NamingScheme
+    {
+        public abstract bool TryConvertToNumber(string str, out short result);
+        public abstract string GetNthName(short ord);
+    }
+
+    public abstract class VariableWidthScheme : NamingScheme
+    {
+        public short width = 0;
+
+        public VariableWidthScheme Clone()
+        {
+            return this.MemberwiseClone() as VariableWidthScheme;
+        }
+    }
+
+    public enum Case { Lower, Upper };
+
+    namespace Scheme
+    {
+        public class Decimal : VariableWidthScheme
+        {
+            public override bool TryConvertToNumber(string str, out short result) => Int16.TryParse(str, out result);
+
+            public override string GetNthName(short ord) => String.Format("{0:D" + width.ToString() + "}", ord);
+        }
+
+        public class Hex : VariableWidthScheme
+        {
+            public Case letterCase;
+
+            public override bool TryConvertToNumber(string str, out short result)
+            {
+                return Int16.TryParse(str, NumberStyles.HexNumber, CultureInfo.InvariantCulture, out result);
+            }
+
+            public override string GetNthName(short ord) => String.Format("{0:" + (letterCase == Case.Upper ? 'X' : 'x' ) + width.ToString() + "}", ord);
+        }
+
+        public class Roman : NamingScheme
+        {
+            private static Dictionary<char, short> FromRoman = new Dictionary<char, short>()
+            {
+                {'I', 1},
+                {'V', 5},
+                {'X', 10},
+                {'L', 50},
+                {'C', 100},
+                {'D', 500},
+                {'M', 1000},
+            };
+
+            private static List<(short, string)> ToRoman = new List<(short, string)>
+            {
+                (1000, "M" ),
+                (900,  "CM"),
+                (500,  "D" ),
+                (400,  "CD"),
+                (100,  "C" ),
+                (90,   "XC"),
+                (50 ,  "L" ),
+                (40 ,  "XL"),
+                (10,   "X" ),
+                (9,    "IX"),
+                (5,    "V" ),
+                (4,    "IV"),
+                (1,    "I" ),
+            };
+
+            public Case letterCase;
+
+            public override bool TryConvertToNumber(string str, out short result)
+            {
+                result = 0;
+                str = str.ToUpper();
+
+                if (!str.All(c => FromRoman.Keys.Contains(c)))
+                    return false;
+
+                for (int i = 0; i < str.Length; ++i)
+                {
+                    if (i + 1 < str.Length && FromRoman[str[i]] < FromRoman[str[i + 1]])
+                    {
+                        result -= FromRoman[str[i]];
+                    }
+                    else
+                    {
+                        result += FromRoman[str[i]];
+                    }
+                }
+                return true;
+            }
+
+            public override string GetNthName(short ord)
+            {
+                if (ord <= 0 || ord >= 4000)
+                    return "";
+
+                var result = new StringBuilder();
+
+                foreach (var (num, str) in ToRoman)
+                {
+                    while (ord >= num)
+                    {
+                        result.Append(letterCase == Case.Lower ? str.ToLower() : str);
+                        ord -= num;
+                    }
+                }
+
+                return result.ToString();
+            }
+        }
+
+        public class LatinAlphabet : NamingScheme
+        {
+            public Case letterCase;
+
+            public override bool TryConvertToNumber(string str, out short result)
+            {
+                result = 0;
+
+                if (str.Length == 1)
+                {
+                    char c = char.ToUpper(str[0]);
+                    if (c >= 'A' && c <= 'Z')
+                    {
+                        result = (short)(c - 'A' + 1);
+                        return true;
+                    }
+                }
+
+                return false;
+            }
+
+            public override string GetNthName(short ord)
+            {
+                if (ord <= 0 || ord > ('Z' - 'A' + 1))
+                    return "";
+
+                char c = letterCase == Case.Upper ? 'A' : 'a';
+                c += (char)(ord - 1);
+
+                return c.ToString();
+            }
+        }
+
+        public class GreekAlphabet : NamingScheme
+        {
+            public Case letterCase;
+
+            private static List<(char, char)> alphabet = new List<(char, char)>
+            {
+                ('Α', 'α'),
+                ('Β', 'β'),
+                ('Γ', 'γ'),
+                ('Δ', 'δ'),
+                ('Ε', 'ε'),
+                ('Ζ', 'ζ'),
+                ('Η', 'η'),
+                ('Θ', 'θ'),
+                ('Ι', 'ι'),
+                ('Κ', 'κ'),
+                ('Λ', 'λ'),
+                ('Μ', 'μ'),
+                ('Ν', 'ν'),
+                ('Ξ', 'ξ'),
+                ('Ο', 'ο'),
+                ('Π', 'π'),
+                ('Ρ', 'ρ'),
+                ('Σ', 'σ'),
+                ('Τ', 'τ'),
+                ('Υ', 'υ'),
+                ('Φ', 'φ'),
+                ('Χ', 'χ'),
+                ('Ψ', 'ψ'),
+                ('Ω', 'ω'),
+            };
+
+            public override bool TryConvertToNumber(string str, out short result)
+            {
+                result = 0;
+
+                if (str.Length != 1)
+                    return false;
+
+                char c = str[0];
+                var idx = alphabet.FindIndex(l => c == l.Item1 || c == l.Item2);
+                if (idx == -1)
+                    return false;
+
+                result = (short)(idx + 1);
+                return true;
+            }
+
+            public override string GetNthName(short ord)
+            {
+                if (ord <= 0 || ord > alphabet.Count)
+                    return "";
+
+                var (upper, lower) = alphabet[ord - 1];
+                char c = letterCase == Case.Upper ? upper : lower;
+                return c.ToString();
+            }
+        }
+
+        public class GreekAlphabetAsWords : NamingScheme
+        {
+            public Case letterCase;
+
+            private static List<string> alphabet = new List<string>
+            {
+                "alpha",
+                "beta",
+                "gamma",
+                "delta",
+                "epsilon",
+                "zeta",
+                "eta",
+                "theta",
+                "iota",
+                "kappa",
+                "lambda",
+                "mu",
+                "nu",
+                "xi",
+                "omicron",
+                "pi",
+                "rho",
+                "sigma",
+                "tau",
+                "upsilon",
+                "phi",
+                "chi",
+                "psi",
+                "omega",
+            };
+
+            public override bool TryConvertToNumber(string str, out short result)
+            {
+                result = 0;
+
+                str = str.ToLower();
+                var idx = alphabet.FindIndex(l => str == l);
+                if (idx == -1)
+                    return false;
+
+                result = (short)(idx + 1);
+                return true;
+            }
+
+            public override string GetNthName(short ord)
+            {
+                if (ord <= 0 || ord > alphabet.Count)
+                    return "";
+
+                var result = alphabet[ord - 1];
+                if (letterCase == Case.Upper)
+                    result = char.ToUpper(result[0]) + result.Substring(1);
+
+                return result;
+            }
+        }
+    }
+}
---
+++ VesselAutoRenamer.cs
@@ -0,0 +1,362 @@
+﻿using KSP.Localization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace VesselAutoRenamer
+{
+    [KSPScenario(ScenarioCreationOptions.AddToAllGames, new GameScenes[] { GameScenes.SPACECENTER, GameScenes.EDITOR, GameScenes.FLIGHT })]
+    public class VesselAutoRenamer : ScenarioModule
+    {
+        public Dictionary<uint, string> nameHistory = new Dictionary<uint, string>{};
+
+        public static Dictionary<char, NamingScheme> Schemes = new Dictionary<char, NamingScheme> {
+            { 'd', new Scheme.Decimal{ } },
+            { 'x', new Scheme.Hex{ letterCase=Case.Lower } },
+            { 'X', new Scheme.Hex{ letterCase=Case.Upper } },
+            { 'r', new Scheme.Roman{ letterCase=Case.Lower } },
+            { 'R', new Scheme.Roman{ letterCase=Case.Upper } },
+            { 'a', new Scheme.LatinAlphabet{ letterCase=Case.Lower } },
+            { 'A', new Scheme.LatinAlphabet{ letterCase=Case.Upper } },
+            { 'g', new Scheme.GreekAlphabet{ letterCase=Case.Lower } },
+            { 'G', new Scheme.GreekAlphabet{ letterCase=Case.Upper } },
+            { 'w', new Scheme.GreekAlphabetAsWords{ letterCase=Case.Lower } },
+            { 'W', new Scheme.GreekAlphabetAsWords{ letterCase=Case.Upper } },
+        };
+
+        public override void OnLoad(ConfigNode node)
+        {
+            Debug.Log("[VesselAutoRenamer] OnLoad", this);
+            base.OnLoad(node);
+
+            foreach (var vessel in node.GetNodes("VESSEL"))
+            {
+                var name = vessel.GetValue("name");
+                uint pid = 0;
+                if (name == null || !vessel.TryGetValue("pid", ref pid) || pid == 0)
+                {
+                    Debug.LogError("[VesselAutoRenamer] Malformed entry in the savefile", this);
+                    continue;
+                }
+
+                nameHistory[pid] = name;
+            }
+
+            if (nameHistory.Count == 0)
+                Debug.Log("[VesselAutoRenamer] No name history found", this);
+        }
+
+        public override void OnSave(ConfigNode node)
+        {
+            Debug.Log("[VesselAutoRenamer] OnSave", this);
+            base.OnSave(node);
+
+            foreach ((uint pid, string name) in nameHistory.Select(x => (x.Key, x.Value)))
+            {
+                var vessel = node.AddNode("VESSEL");
+                vessel.AddValue("pid", pid);
+                vessel.AddValue("name", name);
+            }
+        }
+
+        public override void OnAwake()
+        {
+            GameEvents.OnFlightGlobalsReady.Add(OnFlightGlobalsReady);
+            GameEvents.OnVesselRollout.Add(OnVesselRollout);
+            GameEvents.onVesselRename.Add(OnVesselRename);
+            GameEvents.onVesselsUndocking.Add(OnVesselsUndocking);
+
+#if DEBUG
+            RunTests();
+#endif
+        }
+
+        private void OnDestroy()
+        {
+            GameEvents.OnFlightGlobalsReady.Remove(OnFlightGlobalsReady);
+            GameEvents.OnVesselRollout.Remove(OnVesselRollout);
+            GameEvents.onVesselRename.Remove(OnVesselRename);
+            GameEvents.onVesselsUndocking.Remove(OnVesselsUndocking);
+        }
+
+        protected void OnFlightGlobalsReady(bool ready)
+        {
+            if (ready && nameHistory.Count == 0)
+                PopulateHistory();
+        }
+
+        protected void OnVesselRollout(ShipConstruct ship)
+        {
+            Vessel vessel = FlightGlobals.ActiveVessel;
+            string ogName = vessel.vesselName;
+            string newName = TryFormatName(ogName.Trim());
+
+            if (newName != ogName)
+            {
+                vessel.vesselName = newName;
+                GameEvents.onVesselRename.Fire(new GameEvents.HostedFromToAction<Vessel, string>(vessel, ogName, newName));
+                UpdateNameHistory(vessel);
+            }
+        }
+
+        protected void OnVesselRename(GameEvents.HostedFromToAction<Vessel, string> eventData)
+        {
+            UpdateNameHistory(eventData.host);
+        }
+        protected void OnVesselsUndocking(Vessel a, Vessel b)
+        {
+            UpdateNameHistory(a);
+            UpdateNameHistory(b);
+        }
+
+        public void UpdateNameHistory(Vessel vessel)
+        {
+            if (vessel == null)
+                return;
+
+            nameHistory.Remove(vessel.persistentId);
+
+            switch (vessel.vesselType)
+            {
+                case VesselType.EVA:
+                case VesselType.DeployedScienceController:
+                case VesselType.DeployedSciencePart:
+                case VesselType.DeployedGroundPart:
+                case VesselType.Debris:
+                case VesselType.DroppedPart:
+                case VesselType.Flag:
+                case VesselType.SpaceObject:
+                    return;
+            }
+
+            if (vessel.DiscoveryInfo?.Level != DiscoveryLevels.Owned)
+                return;
+
+            if (vessel.GetDisplayName() == "Untitled Space Craft" || vessel.GetDisplayName() == Localizer.Format("#autoLOC_900530"))
+                return;
+
+            if (vessel.persistentId == 0)
+                return;
+
+            nameHistory[vessel.persistentId] = vessel.GetDisplayName();
+        }
+
+        protected void PopulateHistory()
+        {
+            nameHistory = new Dictionary<uint, string>{};
+
+            foreach (var vessel in FlightGlobals.Vessels)
+                UpdateNameHistory(vessel);
+        }
+
+        protected string TryFormatName(string template)
+        {
+            var prefix = new StringBuilder();
+            var suffix = template;
+            var idx = suffix.IndexOf('%');
+
+            if (idx == -1 || idx == suffix.Length - 1)
+                return template;
+
+            NamingScheme scheme = null;
+
+            do
+            {
+                prefix.Append(suffix.Substring(0, idx));
+                char c = suffix[idx + 1];
+                suffix = suffix.Substring(idx + 2);
+
+                if (c == '%')
+                {
+                    prefix.Append('%');
+                }
+                else
+                {
+                    scheme = GetScheme(c, ref suffix);
+                    if (scheme != null)
+                    {
+                        suffix = suffix.Replace("%%", "%");
+                        return ApplyScheme(scheme, prefix.ToString(), suffix);
+                    }
+                    prefix.Append('%');
+                    prefix.Append(c);
+                }
+
+                idx = suffix.IndexOf('%');
+            }
+            while (idx != -1 && idx < suffix.Length - 1);
+
+            return prefix + suffix;
+        }
+        protected string ApplyScheme(NamingScheme scheme, string prefix, string suffix)
+        {
+            var names = nameHistory.Select(p => p.Value);
+
+            var matchingNames = names;
+            if (prefix.Length > 0)
+                matchingNames = matchingNames.Where(n => n.StartsWith(prefix));
+            if (suffix.Length > 0)
+                matchingNames = matchingNames.Where(n => n.EndsWith(suffix));
+
+            short maxOrd = 0;
+
+            foreach (var name in matchingNames)
+            {
+                var reminder = name.Substring(prefix.Length);
+                reminder = reminder.Substring(0, reminder.Length - suffix.Length);
+
+                if (reminder.Length == 0)
+                {
+                    if (suffix.Length == 0)
+                        maxOrd = Math.Max(maxOrd, (short)1);
+                    continue;
+                }
+
+                if (scheme.TryConvertToNumber(reminder, out var ord))
+                    maxOrd = Math.Max(maxOrd, ord);
+            }
+
+            if (maxOrd == 0)
+            {
+                if (suffix.Length == 0 && names.Any(n => n == prefix.Trim()))
+                    maxOrd = 1;
+            }
+
+            return prefix + scheme.GetNthName(++maxOrd) + suffix;
+        }
+
+        protected static NamingScheme GetScheme(char c, ref string extra)
+        {
+            NamingScheme scheme;
+
+            if (c != '0')
+                return Schemes.TryGetValue(c, out scheme) ? scheme : null;
+
+            int widthLen = 0;
+            string extraCopy = extra;
+
+            while (extraCopy.Length > 0)
+            {
+                c = extraCopy[0];
+                extraCopy = extraCopy.Substring(1);
+
+                if (!char.IsNumber(c))
+                    break;
+
+                ++widthLen;
+            }
+
+            if (widthLen == 0)
+                return null;
+
+            if (Int16.TryParse(extra.Substring(0, widthLen), out var width))
+            {
+                if (Schemes.TryGetValue(c, out scheme))
+                {
+                    var variableWidthScheme = scheme as VariableWidthScheme;
+                    if (variableWidthScheme != null)
+                    {
+                        var clone = variableWidthScheme.Clone();
+                        clone.width = width;
+                        extra = extraCopy;
+                        return clone;
+                    }
+                }
+            }
+
+            return null;
+        }
+
+#if DEBUG
+        private void RunTests()
+        {
+            nameHistory = new Dictionary<uint, string>
+            {
+                { 1, "Foo" },
+                { 2, "NumA 1" },
+                { 3, "NumB 1" },
+                { 4, "NumB 2" },
+                { 5, "NumC 0" },
+                { 6, "NumD 0002" },
+                { 7, "NumE 2137" },
+                { 8, "23 NumF" },
+                { 9, "42" },
+                { 10, "2 NumG 3" },
+                { 11, "NumH 5X" },
+                { 12, "NumI 023" },
+                { 13, "RomA I" },
+                { 14, "RomB III" },
+                { 15, "RomC iv" },
+                { 16, "RomD XX" },
+                { 17, "RomE XXX" },
+                { 18, "RomF DCCCXCIV" }, // 894
+                { 19, "HexA AA" },
+                { 20, "HexB 01f" },
+                { 21, "HexC 0f0" },
+                { 22, "LatA A" },
+                { 23, "LatB-f" },
+                { 24, "LatC-5G" },
+                { 25, "GreA Α" },
+                { 26, "GreB λ" },
+                { 27, "GreC Alpha" },
+                { 28, "GreD zeta" },
+                { 29, "es%cape 5%" },
+            };
+
+            Action<string, string> test = (format, expected) =>
+            {
+                var value = TryFormatName(format);
+                if (value != expected)
+                    Debug.LogError($"Assertion Failed: \"{value}\" != \"{expected}\"");//throw new Exception($"Assertion Failed: \"{value}\" != \"{expected}\"");
+            };
+
+            test("", "");
+            test("some", "some");
+            test("percent%", "percent%");
+            test("escape%%", "escape%");
+            test("%%escape", "%escape");
+            test("es%%ca%%pe", "es%ca%pe");
+            test("Foo %d", "Foo 2");
+            test("Foo%d", "Foo2");
+            test("Bar %d", "Bar 1");
+            test("NumA %d", "NumA 2");
+            test("NumB %d", "NumB 3");
+            test("NumC %d", "NumC 1");
+            test("NumD %d", "NumD 3");
+            test("NumD %05d", "NumD 00003");
+            test("NumE %d", "NumE 2138");
+            test("%d NumF", "24 NumF");
+            test("%d", "43");
+            test("%d NumG 3", "3 NumG 3");
+            test("2 NumG %d", "2 NumG 4");
+            test("%d NumG %d", "1 NumG %d");
+            test("NumH %dX", "NumH 6X");
+            test("NumI %d", "NumI 24");
+            test("NumI %01d", "NumI 24");
+            test("NumI %03d", "NumI 024");
+            test("NumI %0 d", "NumI %0 d");
+            test("RomA %R", "RomA II");
+            test("RomB %r", "RomB iv");
+            test("RomC %R", "RomC V");
+            test("RomD %R", "RomD XXI");
+            test("RomE %RX", "RomE XXIX");
+            test("RomF %R", "RomF DCCCXCV");
+            test("HexA %x", "HexA ab");
+            test("HexB %X", "HexB 20");
+            test("HexC %03X", "HexC 0F1");
+            test("LatA %a", "LatA b");
+            test("LatB-%A", "LatB-G");
+            test("LatC-5%A", "LatC-5H");
+            test("GreA %g", "GreA β");
+            test("GreB %G", "GreB Μ");
+            test("GreC %W", "GreC Beta");
+            test("GreD %w", "GreD eta");
+            test("es%%cape %d%%", "es%cape 6%");
+
+            nameHistory.Clear();
+        }
+#endif
+    }
+}
